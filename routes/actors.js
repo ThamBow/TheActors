474996840049c624d995e79d00dc7c6519deb8ee
Thamsanqa Moyo <modav497@aol.com>
2019-06-30 15:22:24 +0100

    Initial Actor Routes

---
+++ routes/actors.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const router = express.Router()
+const Actor = require('../models/actor')
+
+//All actors route
+router.get('/', async (req, res) => {
+
+ let searchOptions = {}
+
+ if (req.query.name != null && req.query.name !== ''){
+    searchOptions.name = new RegExp(req.query.name, 'i')
+ }
+
+    try {
+
+    const actors = await Actor.find(searchOptions)
+    res.render('actors/index', {
+        actors: actors,
+        searchOptions: req.query})
+    } catch (err) {
+
+    res.redirect('/')
+    
+
+    }
+
+    
+})
+
+//New actors route
+router.get('/new', (req, res) => {
+    res.render('actors/new', {
+        actor: new Actor()
+    })
+})
+
+
+//Create actor route
+router.post('/', async (req, res ) => {
+    const actor =  new Actor({
+        name: req.body.name
+    })
+  
+    try {
+        const newActor = await actor.save({})
+        //res.redirect(`actors/${newActor.id}`)
+        res.redirect(`actors`)
+
+    } catch (err){
+        
+        res.render('actors/new', {
+            actor: actor,
+            errorMessage:'Error creating Actor!'
+        
+        })
+      
+    }
+    
+})
+module.exports = router
+
+
+
+
+
+
+
+
+
+/* let actor = new Actor();
+    
+   name = req.body.name;
+  
+    actor.save(function(err, newActor){
+
+        if(err){
+            errorMessage: 'Error creating Actor!'
+            return;
+        }
+
+        else {
+           //res.redirect(`actors/${newActor.id}`)
+           actor: actor,
+           res.redirect(`actors`)
+        }
+    }) */
--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 if (process.env.NODE_ENV !== 'production')
 {
-    require('dotenv').config()
+   require('dotenv').config() 
 }
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+
 
 const indexRouter = require('./routes/index')
+const actorRouter = require('./routes/actors')
+
+
+
 
 //Connect to database
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {
+
+
+
+mongoose.connect( process.env.DATABASE_URL, {
     useNewUrlParser: true
 })
+
+
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose!'))
@@ -22,11 +35,17 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 
-app.use(expressLayouts)
+app.use(expressLayouts )
 app.use(express.static('public'))
-// Possible mongoose line
-app.use('/', indexRouter)
+app.use(express.json())
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false }))
 
+
+// Possible mongoose line
+
+//Point to router in question
+app.use('/', indexRouter)//Points to index.js in the root folder
+app.use('/actors', actorRouter)//Views folder/actors
 
 
 app.listen(process.env.PORT || 3000)
